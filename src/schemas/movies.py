fd583bf97c3288c5b181484076adb67ab42fd2b4
Vitalii <vetali5700gmail.com>
2025-04-17 23:35:21 +0300

    add MovieDetailResponseSchema in schemas/movies.py

--- src/schemas/movies.py
+++ src/schemas/movies.py
@@ -1,1 +1,23 @@
 # Write your code here
+import datetime
+
+from pydantic import BaseModel
+
+class MovieDetailResponseSchema(BaseModel):
+    id: int
+    name: str = None
+    price: float = None
+    date: datetime.date = None
+    score: float = None
+    genre: str = None
+    overview: str = None
+    crew: str = None
+    orig_title: str = None
+    status: str = None
+    orig_lang: str = None
+    budget: float = None
+    revenue: float = None
+    country: str = None
+
+    class Config:
+        from_attributes = True
