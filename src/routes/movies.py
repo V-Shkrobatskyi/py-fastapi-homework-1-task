1b39920cca52b64220cac11d341791800d7b7e86
Vitalii <vetali5700gmail.com>
2025-04-17 23:36:22 +0300

    add route for movie details view in routes/movies.py

--- src/routes/movies.py
+++ src/routes/movies.py
@@ -1,10 +1,17 @@
-from fastapi import APIRouter, Depends, HTTPException, Query
-from sqlalchemy import select
+from fastapi import APIRouter, Depends, HTTPException, Query, Request
 from sqlalchemy.ext.asyncio import AsyncSession
-
-from database import get_db, MovieModel
-
+from sqlalchemy import select, func
+from database.models import MovieModel
+from schemas.movies import MovieDetailResponseSchema, MovieListResponseSchema
+from database import get_db
 
 router = APIRouter()
 
 # Write your code here
+@router.get("/movies/{movie_id}/", response_model=MovieDetailResponseSchema)
+async def get_movies(movie_id: int, db: AsyncSession = Depends(get_db)):
+    result = await db.execute(select(MovieModel).where(MovieModel.id == movie_id))
+    movie = result.scalar_one_or_none()
+    if not movie:
+        raise HTTPException(status_code=404, detail="Movie with the given ID was not found.")
+    return movie
